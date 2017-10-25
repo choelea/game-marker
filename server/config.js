62d8ca0745a2616c0270c97d14b19bfdaef8c0f6
Joe Li <joe.li@okchem.com>
2017-10-25 10:56:56 +0800

    clean  configuration and  remove some condidential configuration to cloud

--- server/config.js
+++ server/config.js
@@ -2,28 +2,8 @@ const CONF = {
     port: '5757',
     rootPathname: '',
 
-    // 微信小程序 App ID
-    appId: 'wxf2383817a3ac1f8d',
-
-    // 微信小程序 App Secret
-    appSecret: '24198aa7b1196970939633b2d673863b',
-
     // 是否使用腾讯云代理登录小程序
     useQcloudLogin: true,
-
-    /**
-     * MySQL 配置，用来存储 session 和用户信息
-     * 若使用了腾讯云微信小程序解决方案
-     * 开发环境下，MySQL 的初始密码为您的微信小程序 appid
-     */
-    mysql: {
-        host: 'localhost',
-        port: 3306,
-        user: 'root',
-        pass: 'Game-Marker!123',
-        db: 'cAuth',
-        char: 'utf8mb4'
-    },
 
     cos: {
         /**
@@ -45,15 +25,6 @@ const CONF = {
     // 微信登录态有效期
     wxLoginExpires: 7200,
 
-    // 其他配置 ...
-    serverHost: 'www.yuyoucloud.com',
-    tunnelServerUrl: 'http://tunnel.ws.qcloud.la',
-    tunnelSignatureKey: '27fb7d1c161b7ca52d73cce0f1d833f9f5b5ec89',
-    // 腾讯云相关配置可以查看云 API 秘钥控制台：https://console.qcloud.com/capi
-    qcloudAppId: '1251678573',
-    qcloudSecretId: 'AKIDsdEdqr3GftuiJhrreBYnqhLn380fGPWV',
-    qcloudSecretKey: 'vgkiW8YllY9aoqfHl3BA0eHqGUXN0mUz',
-    wxMessageToken: 'weixinmsgtoken',
     networkTimeout: 30000
 }
 module.exports = process.env.NODE_ENV === 'local' ? Object.assign({}, CONF, require('./config.local')) : CONF
