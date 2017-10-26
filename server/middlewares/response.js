26188c687c228b34345ee73f8812f4d3b7ccd986
Joe Li <joe.li@okchem.com>
2017-10-26 09:44:54 +0800

    Send 404 if there is not route found

--- server/middlewares/response.js
+++ server/middlewares/response.js
@@ -11,15 +11,15 @@ module.exports = async (ctx, next) => {
         // 处理响应结果
         // 如果直接写入在 body 中，则不作处理
         // 如果写在 ctx.body 为空，则使用 state 作为响应
-        if (ctx.status === 404) {
-            ctx.throw(404, 'The resource you requested does not exit')
+        if (ctx.matched.length < 0) {
+            ctx.throw(404, `Not route founded for the given URI:${ctx.url}`)
         }
         ctx.body = ctx.body ? ctx.body : {
             code: ctx.state.code !== undefined ? ctx.state.code : 0,
             data: ctx.state.data !== undefined ? ctx.state.data : {}
         }
     } catch (e) {
-        console.log('error ---------------------- '+e)
+        console.log('error ---------------------- ' + e)
         // catch 住全局的错误信息
         debug('Catch Error: %o', e)
         // 设置状态码为 500 - 服务端错误
