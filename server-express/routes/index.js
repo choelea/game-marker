0cd7afc65faa187bdaac53f02b0ba81b7eed70b9
Joe Li <joe.li@okchem.com>
2017-11-01 14:18:24 +0800

    Add api for GET /login

--- server-express/routes/index.js
+++ server-express/routes/index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const login = require('../controllers/login')
 const game = require('../controllers/game')
+const { authorization } = require('../middlewares/qcloud')
 
 const router = express.Router()
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.json({ title: 'Home Page' })
-})
+router.get('/', (req, res) => res.json({ title: 'Home Page' }))
 
+router.get('/login', authorization, login)
 router.post('/games', game.post)
 router.get('/games/:id', game.get)
 module.exports = router
