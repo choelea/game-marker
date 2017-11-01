adb31380f5674ba714f3a5b5bccc3ad8e1ba7f77
Joe Li <joe.li@okchem.com>
2017-11-01 15:20:19 +0800

    initdb

---
+++ server-express/tools/cAuth.sql
@@ -0,0 +1,92 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : Localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50717
+ Source Host           : localhost
+ Source Database       : cAuth
+
+ Target Server Type    : MySQL
+ Target Server Version : 50717
+ File Encoding         : utf-8
+
+ Date: 08/10/2017 22:22:52 PM
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `cSessionInfo`
+-- ----------------------------
+DROP TABLE IF EXISTS `cSessionInfo`;
+CREATE TABLE `cSessionInfo` (
+  `open_id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `uuid` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `skey` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_visit_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `session_key` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_info` varchar(2048) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`open_id`),
+  KEY `openid` (`open_id`) USING BTREE,
+  KEY `skey` (`skey`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='会话管理用户信息';
+
+DROP TABLE IF EXISTS `game`;
+CREATE TABLE `game` (
+  `name` varchar(20) COLLATE utf8_bin NOT NULL,
+  `minScore` int(11) NOT NULL,
+  `owner` varchar(50) COLLATE utf8_bin NOT NULL,
+  `records` json DEFAULT NULL,
+  `createdBy` varchar(50) COLLATE utf8_bin NOT NULL,
+  `createdTS` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updatedBy` varchar(50) COLLATE utf8_bin DEFAULT NULL,
+  `updatedTS` timestamp NULL DEFAULT NULL,
+  `id` varchar(50) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gamemember`
+--
+DROP TABLE IF EXISTS `gamemember`;
+CREATE TABLE `gamemember` (
+  `userid` varchar(50) COLLATE utf8_bin NOT NULL,
+  `game` varchar(50) COLLATE utf8_bin NOT NULL,
+  `username` varchar(100) COLLATE utf8_bin NOT NULL,
+  `avatarUrl` varchar(255) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `game`
+--
+ALTER TABLE `game`
+  ADD UNIQUE KEY `game-id` (`id`),
+  ADD KEY `game-owner` (`owner`);
+
+--
+-- Indexes for table `gamemember`
+--
+ALTER TABLE `gamemember`
+  ADD KEY `game-memeber-userid` (`userid`),
+  ADD KEY `FK-gamememeber-game` (`game`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `gamemember`
+--
+ALTER TABLE `gamemember`
+  ADD CONSTRAINT `FK-gamememeber-game` FOREIGN KEY (`game`) REFERENCES `game` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
