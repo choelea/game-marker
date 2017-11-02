a7fa8526e465d648a670d363386956f488f896b4
Joe Li <joe.li@okchem.com>
2017-11-02 08:50:19 +0800

    debug

--- server-express/controllers/login.js
+++ server-express/controllers/login.js
@@ -1,11 +1,11 @@
 
 
 // 登录授权接口
-module.exports = async (req, res) => {
+module.exports = (req, res) => {
   // 通过 Koa 中间件进行登录之后
   // 登录信息会被存储到 ctx.state.$wxInfo
   // 具体查看：
-  if (req.$wxInfo.loginState) {
+  if (req.$wxInfo && req.$wxInfo.loginState) {
     res.json({ data: req.$wxInfo.userInfo })
   }
   res.status(401).json({ code: '-1' })
